ed2c9a041714afa65daa8f131db988f6bf98a155
Jacques DAI <63266015+DLoutres@users.noreply.github.com>
2020-12-16 14:54:54 +0100

    Create jenkins.develop.groovy

---
+++ jenkins.develop.groovy
@@ -0,0 +1,20 @@
+def build_app(){
+  
+}
+
+def test_app(){
+  bat 'python test_app.py'
+}
+
+def down_app(){
+  
+}
+
+def release_app(){
+  echo 'branch into release'
+}
+
+def live_app(){
+}
+
+return this
