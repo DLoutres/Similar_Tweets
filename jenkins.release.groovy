7d0d999d5385350f31b80a8ecc7a210a10dcfe99
Jacques DAI <63266015+DLoutres@users.noreply.github.com>
2020-12-16 16:16:40 +0100

    Update jenkins.release.groovy

--- jenkins.release.groovy
+++ jenkins.release.groovy
@@ -2,8 +2,16 @@ def build_app(){
   sh 'docker-compose up -d'
 }
 
+def e2e_test(){
+  echo 'release-specific testing here'
+}
+def user_acceptance(){
+  input "proceed with deployment to live?"
+}
+
 def test_app(){
-  sh 'python test_app.py'
+  e2e_test()
+  user_acceptance()
 }
 
 def down_app(){
@@ -11,10 +19,10 @@ def down_app(){
 }
 
 def release_app(){
-  echo 'branch into release'
 }
 
 def live_app(){
+  echo 'merge with main'
 }
 
 return this
